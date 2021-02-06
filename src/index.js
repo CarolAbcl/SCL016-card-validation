c339fd609bfb55fef9acb152892044a7a5ac86b3
unknown <carolallende@gmail.com>
2021-02-06 18:52:43 -0300

    Inicio JavaScript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import validator from './validator.js';
-
-console.log(validator);
+function myFunction() {
+  alert("pasa");
+  document.getElementById('alertInvalid').style.display='block';  
+}
+//let creditCardNumber = document.getElementById("numberCard").value;
+//console.log(creditCardNumber);
+//import validator from './validator.js';
+//console.log(validator);*/
